c7e3a2603c59711d03eb0a12bb95933205a19c84
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-11-24 22:06:02 -0500

    Add users utils file, add adduser and delete user functions

---
+++ src/utils/users.js
@@ -0,0 +1,47 @@
+const users = [];
+
+// Add user
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    // validate the data
+    if (username === '' || room === '') {
+        return {
+            error: 'Username and room is required!',
+        };
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username;
+    });
+
+    // validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!',
+        };
+    }
+
+    // Store user
+    const newUser = { id, username, room };
+    users.push(newUser);
+
+    return {
+        newUser,
+    };
+};
+
+// Remove user
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+    if (index !== -1) {
+        return users.splice(index, 1);
+    }
+};
+
+// getUser
+
+// getUsersInRoom
